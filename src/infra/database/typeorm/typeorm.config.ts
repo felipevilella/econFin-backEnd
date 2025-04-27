dea65f5d95f73d1043d913f89a37aeba28746a02
Felipe Vilella <felipevilellaalves@hotmail.com>
2025-04-27 15:13:24 -0300

    fix: ajust fix typeorm

--- src/infra/database/typeorm/typeorm.config.ts
+++ src/infra/database/typeorm/typeorm.config.ts
@@ -2,27 +2,30 @@ import { DataSource } from "typeorm";
 import * as dotenv from "dotenv";
 dotenv.config();
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const config = new DataSource({
   type: "postgres",
-  host: process.env.DATABASE_HOST,
-  port: Number(process.env.DATABASE_PORT),
-  username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  migrationsRun: process.env.DATABASE_MIGRATIONS === "true",
-  logging: process.env.DATABASE_LOGGING === "true",
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || "5432"),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   synchronize: true,
+  migrationsRun: false,
+  logging: true,
   migrations: ["database/migrations/**/*{.ts,.js}"],
   entities: [__dirname + "./../../**/*.entity{.ts,.js}"],
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
 
 config
   .initialize()
   .then(() => {
-    console.log("Data Source has been initialized!");
+    console.log("✅ Data Source has been initialized!");
   })
   .catch((err) => {
-    console.error("Error during Data Source initialization", err);
+    console.error("❌ Error during Data Source initialization", err);
   });
 
 export default config;
