71e5cef46ff6306ad53c52fcc3b8fde37d0a5a3c
Felipe Vilella <felipevilellaalves@hotmail.com>
2025-04-28 14:51:16 -0300

    fix: ajust cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(
     cors({
-      origin: "http://localhost:3000",
+      origin: "*",
       methods: ["GET", "POST", "PUT", "DELETE"],
       allowedHeaders: ["Content-Type", "Authorization"],
     }),
